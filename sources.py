0c5115c1c359e1af346faa2a9a0e37cf85d98021
dtmity <111884694+dtmity@users.noreply.github.com>
2024-12-08 14:18:25 +0300

    Added conf for sources 1/3

--- sources.py
+++ sources.py
@@ -2,3 +2,5 @@ sources = {('https://lenta.ru/rss/news', 'lenta'),
                ('https://russian.rt.com/rss', 'rt'),
                ('https://www.vedomosti.ru/rss/news.xml', 'ведомости'),
                ('https://ria.ru/export/rss2/index.xml', 'ria')}
+
+channels_name = {'oldlentach', 'rian_ru', 'breakingmash'}
